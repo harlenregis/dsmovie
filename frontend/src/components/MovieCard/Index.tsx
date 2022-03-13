e5c4729c3ff838264224c7ebabb4c4d3215d19c7
Harlen Regis <harlenregis@hotmail.com>
2022-03-13 12:15:04 -0300

    Pagination

--- frontend/src/components/MovieCard/Index.tsx
+++ frontend/src/components/MovieCard/Index.tsx
@@ -1,12 +1,12 @@
 import MovieScore from "components/MovieScore";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
 type Props = {
     movie: Movie;
 }
 
-function MovieCard({movie}:Props) {
+function MovieCard({ movie }: Props) {
 
     return (
 
